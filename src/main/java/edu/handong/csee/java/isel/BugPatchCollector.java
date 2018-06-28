695c0f411ec30c850405f08db44f36a033d6e9e7
sungbin <21700613@handong.edu>
2018-06-28 18:04:29 +0900

    fit git-diff

--- src/main/java/edu/handong/csee/java/isel/BugPatchCollector.java
+++ src/main/java/edu/handong/csee/java/isel/BugPatchCollector.java
@@ -1,10 +1,27 @@
 package edu.handong.csee.java.isel;
 
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
 public class BugPatchCollector {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		File directory = new File("/Users/imseongbin/documents/Java/BugPatchCollector");
+		
+		
+		try {
+		
+		Git git = Git.open(directory);
+		Repository repository = git.getRepository();
+		
+		git.diff().setOutputStream( System.out ).call();
+		
+		
+		
+		
+		} catch (Exception e) {System.out.println(e.fillInStackTrace());}
 	}
 
 }
