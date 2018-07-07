7a14203a265ef505af735973b07220bb06674071
sungbin <21700613@handong.edu>
2018-07-07 22:56:12 +0900

    complete example of parse git-repository

--- src/main/java/edu/handong/csee/isel/java/BugPatchCollector.java
+++ src/main/java/edu/handong/csee/isel/java/BugPatchCollector.java
@@ -5,10 +5,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.diff.DiffEntry;
@@ -16,6 +21,7 @@ import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
@@ -32,255 +38,49 @@ import org.eclipse.jgit.errors.RevisionSyntaxException;
 public class BugPatchCollector {
 
 	public static void main(String[] args) {
-		File directory = new File("/Users/imseongbin/documents/Java/BugPatchCollector");
+		String directoryPath = "/Users/imseongbin/documents/Java/BugPatchCollector";
+		File directory = new File(directoryPath);
 		BugPatchCollector bc = new BugPatchCollector();
-		String selectedBranch = "";
-
-		// selectedBranch = "Test2";
 
 		/* 필요한게 Branch List, All path List, Commit-hash List, */
 
+		ArrayList<String> branchList = new ArrayList<String>();
+		HashMap<String, ArrayList<String>> commitHashList = new HashMap<String, ArrayList<String>>(); // <branch, commits>
+		HashMap<String, ArrayList<String>> allPathList = new HashMap<String, ArrayList<String>>(); // <commit, paths>
+
 		try {
 
-			Git git = Git.open(new File(directory.toString() + "/.git"));
+			Git git = Git.open(new File(directoryPath + "/.git"));
 			Repository repository = git.getRepository();
 
-			Iterable<RevCommit> logs = git.log().call();
+			Patch p = new Patch(directoryPath);
+			branchList = p.getBranchList();
+			allPathList = p.getAllPathList();
 
-			String oldCommitHash = "45c754d81e119a4b1d51116454b9717e64c77be7";
-			String newCommitHash = "47eb740a401d0d2cb8b6423e0af2bff8a51dfb4a";
+			for (String branch : branchList) {
+				p.setCommitHashs(branch);
+			}
 
-			List<String> paths = bc.readElementsAt(repository, oldCommitHash,
-					"src/main/java/edu/handong/csee/isel/java");
+			System.out.println("Successe");
 
-			System.out.println("Had paths for commit: " + paths);
+			commitHashList = p.getCommitHashs();
+			Set<Entry<String, ArrayList<String>>> set = commitHashList.entrySet();
+			Iterator<Entry<String, ArrayList<String>>> it = set.iterator();
+			while (it.hasNext()) {
+				Map.Entry<String, ArrayList<String>> e = (Map.Entry<String, ArrayList<String>>) it.next();
+				System.out.println("branch: " + e.getKey());
+				ArrayList<String> pathList = new ArrayList<String>();
+				for (String commitHash : e.getValue()) {
+					System.out.println("	commitHash: " + commitHash);
+					pathList = p.getPathList(commitHash);
+					if (pathList.isEmpty())
+						continue;
 
-			/* print patch between two commits */
-			// bc.printPatch(oldCommitHash, newCommitHash, repository);
-
-			/* Commit message */
-			// ArrayList<String> messages = bc.getCommitMessages(git);
-			// for (String message : messages) {
-			// System.out.println(message);
-			// }
-			/* git-diff */
-			// bc.printGitDiff(git);
-
-			/* pull commits list and print them */
-			// bc.printLog(logs, repository, selectedBranch);
-
-			/* commit Hash */
-			// ArrayList<String> commitHashList = new ArrayList<String>();
-			// commitHashList = bc.loadCommitHash(logs, repository, selectedBranch);
-			// System.out.println("Commit Hash: ");
-			// for (String hash : commitHashList) {
-			// System.out.println(hash);
-			// }
-
-			/* commit Message */
-			// ArrayList<String> commitMessageList = new ArrayList<String>();
-			// commitMessageList = bc.loadCommitMessages(logs, repository, selectedBranch);
-			// System.out.println("Commit Messages: ");
-			// for(String message : commitMessageList) {
-			// System.out.println(message);
-			// }
-
+					allPathList.put(commitHash, pathList);
+				}
+			}
 		} catch (Exception e) {
-			System.out.println(e.fillInStackTrace());
-		}
-	}
-
-	private static RevCommit buildRevCommit(Repository repository, String commit) throws IOException {
-		// a RevWalk allows to walk over commits based on some filtering that is defined
-		try (RevWalk revWalk = new RevWalk(repository)) {
-			return revWalk.parseCommit(ObjectId.fromString(commit));
-		}
-	}
-
-	private static TreeWalk buildTreeWalk(Repository repository, RevTree tree, final String path) throws IOException {
-		TreeWalk treeWalk = TreeWalk.forPath(repository, path, tree);
-
-		if (treeWalk == null) {
-			throw new FileNotFoundException(
-					"Did not find expected file '" + path + "' in tree '" + tree.getName() + "'");
-		}
-
-		return treeWalk;
-	}
-
-	private List<String> readElementsAt(Repository repository, String commit, String path) throws IOException {
-		RevCommit revCommit = buildRevCommit(repository, commit);
-
-		// and using commit's tree find the path
-		RevTree tree = revCommit.getTree();
-		// System.out.println("Having tree: " + tree + " for commit " + commit);
-
-		List<String> items = new ArrayList<>();
-
-		// shortcut for root-path
-		if (path.isEmpty()) {
-			try (TreeWalk treeWalk = new TreeWalk(repository)) {
-				treeWalk.addTree(tree);
-				treeWalk.setRecursive(false);
-				treeWalk.setPostOrderTraversal(false);
-
-				while (treeWalk.next()) {
-					items.add(treeWalk.getPathString());
-				}
-			}
-		} else {
-			// now try to find a specific file
-			try (TreeWalk treeWalk = buildTreeWalk(repository, tree, path)) {
-				if ((treeWalk.getFileMode(0).getBits() & FileMode.TYPE_TREE) == 0) {
-					throw new IllegalStateException("Tried to read the elements of a non-tree for commit '" + commit
-							+ "' and path '" + path + "', had filemode " + treeWalk.getFileMode(0).getBits());
-				}
-
-				try (TreeWalk dirWalk = new TreeWalk(repository)) {
-					dirWalk.addTree(treeWalk.getObjectId(0));
-					dirWalk.setRecursive(false);
-					while (dirWalk.next()) {
-						items.add(dirWalk.getPathString());
-					}
-				}
-			}
-		}
-
-		return items;
-	}
-
-	private void printPatch(String oldCommitHash, String newCommitHash, Repository repository)
-			throws IOException, GitAPIException {
-
-		Git git = new Git(repository);
-
-		AbstractTreeIterator oldTreeParser = prepareTreeParser(repository, oldCommitHash);
-		AbstractTreeIterator newTreeParser = prepareTreeParser(repository, newCommitHash);
-
-		List<DiffEntry> diff = git.diff().setOldTree(oldTreeParser).setNewTree(newTreeParser)
-				.setPathFilter(PathFilter.create("src/main/java/edu/handong/csee/isel/java/TestClass.java")).
-				// to filter on Suffix use the following insteadF
-				// setPathFilter(PathSuffixFilter.create(".java")).
-				call();
-		for (DiffEntry entry : diff) {
-			System.out.println("Entry: " + entry + ", from: " + entry.getOldId() + ", to: " + entry.getNewId());
-			try (DiffFormatter formatter = new DiffFormatter(System.out)) {
-				formatter.setRepository(repository);
-				formatter.format(entry);
-			}
-		}
-
-	}
-
-	private ArrayList<String> loadCommitHash(Iterable<RevCommit> logs, Repository repository, String selectedBranch)
-			throws RevisionSyntaxException, NoHeadException, MissingObjectException, IncorrectObjectTypeException,
-			AmbiguousObjectException, GitAPIException, IOException {
-		Git git = new Git(repository);
-		ArrayList<String> commitHashList = new ArrayList<String>();
-
-		int count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-			count++;
-		}
-
-		logs = git.log().add(repository.resolve("remotes/origin/" + selectedBranch)).call();
-		count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-
-			commitHashList.add(rev.name());
-			count++;
-		}
-		return commitHashList;
-	}
-
-	private ArrayList<String> loadCommitMessages(Iterable<RevCommit> logs, Repository repository, String selectedBranch)
-			throws RevisionSyntaxException, NoHeadException, MissingObjectException, IncorrectObjectTypeException,
-			AmbiguousObjectException, GitAPIException, IOException {
-
-		Git git = new Git(repository);
-		ArrayList<String> commitMessageList = new ArrayList<String>();
-
-		int count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-			count++;
-		}
-
-		logs = git.log().add(repository.resolve("remotes/origin/" + selectedBranch)).call();
-		count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-
-			commitMessageList.add(rev.getShortMessage());
-			count++;
-		}
-		return commitMessageList;
-	}
-
-	private void printLog(Iterable<RevCommit> logs, Repository repository, String selectedBranch)
-			throws RevisionSyntaxException, NoHeadException, MissingObjectException, IncorrectObjectTypeException,
-			AmbiguousObjectException, GitAPIException, IOException {
-		Git git = new Git(repository);
-
-		int count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-			count++;
-		}
-		System.out.println("Had " + count + " commits overall on " + selectedBranch + " branch");
-
-		logs = git.log().add(repository.resolve("remotes/origin/" + selectedBranch)).call();
-		count = 0;
-		for (RevCommit rev : logs) {
-			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
-			// " + rev.getId().getName() */);
-
-			System.out.println("Message: " + rev.getShortMessage());
-			System.out.println("Commit: " + rev.name());
-			count++;
-		}
-
-	}
-
-	private ArrayList<String> getCommitMessages(Git git) throws Exception {
-		ArrayList<String> commitMessages = new ArrayList<String>();
-
-		Iterable<RevCommit> log = git.log().call();
-		for (Iterator<RevCommit> iterator = log.iterator(); iterator.hasNext();) {
-			RevCommit rev = iterator.next();
-			commitMessages.add(rev.getFullMessage());
-		}
-
-		return commitMessages;
-	}
-
-	private void printGitDiff(Git git) throws GitAPIException {
-		git.diff().setOutputStream(System.out).call();
-	}
-
-	private static AbstractTreeIterator prepareTreeParser(Repository repository, String objectId) throws IOException {
-		// from the commit we can build the tree which allows us to construct the
-		// TreeParser
-		// noinspection Duplicates
-		try (RevWalk walk = new RevWalk(repository)) {
-			RevCommit commit = walk.parseCommit(ObjectId.fromString(objectId));
-			RevTree tree = walk.parseTree(commit.getTree().getId());
-
-			CanonicalTreeParser treeParser = new CanonicalTreeParser();
-			try (ObjectReader reader = repository.newObjectReader()) {
-				treeParser.reset(reader, tree.getId());
-			}
-
-			walk.dispose();
-
-			return treeParser;
+			e.printStackTrace();
 		}
 	}
 }
--- src/main/java/edu/handong/csee/isel/java/Patch.java
+++ src/main/java/edu/handong/csee/isel/java/Patch.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import org.eclipse.jgit.api.Git;
@@ -29,12 +30,39 @@ import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
 
 public class Patch {
+
 	String directoryPath;
 	File directory;
 	Git git;
 	Repository repository;
-	ArrayList<String> commitHashList = new ArrayList<String>();
+	HashMap<String, ArrayList<String>> commitHashs; // <branch, commitHash>
+	HashMap<String, ArrayList<String>> allPathList = new HashMap<String, ArrayList<String>>(); // <commitHash, pathList>
 	ArrayList<String> branchList = new ArrayList<String>();
+
+	public HashMap<String, ArrayList<String>> getCommitHashs() {
+		return commitHashs;
+	}
+
+	public void setCommitHashs(String branch) throws RevisionSyntaxException, NoHeadException, MissingObjectException,
+			IncorrectObjectTypeException, AmbiguousObjectException, GitAPIException, IOException {
+		if (branchList.isEmpty() || !branchList.contains(branch)) {
+			System.out.println("branch is not!");
+			return;
+		}
+		ArrayList<String> commitHashList = new ArrayList<String>();
+
+		/**/
+		Iterable<RevCommit> logs = git.log().call();
+		logs = git.log().add(repository.resolve(branch)) // 여길 remotes로 할지, head로 해야할지 잘 모르겠음.
+				.call();
+		for (RevCommit rev : logs) {
+
+			commitHashList.add(rev.getId().getName());
+
+		}
+		this.commitHashs.put(branch, commitHashList);
+
+	}
 
 	public void setBranchList() throws GitAPIException {
 		List<Ref> call = git.branchList().call();
@@ -47,21 +75,18 @@ public class Patch {
 		return branchList;
 	}
 
-	public ArrayList<String> getAllPathList() {
+	public HashMap<String, ArrayList<String>> getAllPathList() { // parameter: commitHash
 		// TODO Auto-generated method stub
-		return null;
+		return allPathList;
 	}
 
-	public ArrayList<String> getCommitHashList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Patch(String directoryPath) throws IOException {
+	public Patch(String directoryPath) throws IOException, GitAPIException {
+		this.commitHashs = new HashMap<String, ArrayList<String>>();
 		this.directoryPath = directoryPath;
 		this.directory = new File(directoryPath);
 		this.git = Git.open(new File(directoryPath + "/.git"));
 		this.repository = this.git.getRepository();
+		this.setBranchList();
 	}
 
 	public void reset() {
@@ -69,13 +94,37 @@ public class Patch {
 		this.directory = null;
 		this.git = null;
 		this.repository = null;
+		this.branchList = null;
 	}
 
-	public void set(String directoryPath) throws IOException {
+	public void set(String directoryPath) throws IOException, GitAPIException {
+		this.commitHashs = new HashMap<String, ArrayList<String>>();
 		this.directoryPath = directoryPath;
 		this.directory = new File(directoryPath);
 		this.git = Git.open(new File(directoryPath + "/.git"));
 		this.repository = this.git.getRepository();
+		this.setBranchList();
+	}
+
+	public ArrayList<String> getPathList(String commitHash) throws IOException {
+		ArrayList<String> pathList = new ArrayList<String>();
+
+		RevWalk walk = new RevWalk(repository);
+		ObjectId id = repository.resolve(commitHash);
+		RevCommit commit = walk.parseCommit(id);
+
+		RevTree tree = commit.getTree();
+		TreeWalk treeWalk = new TreeWalk(repository);
+		treeWalk.addTree(tree);
+		treeWalk.setRecursive(true);
+		while (treeWalk.next()) {
+			pathList.add(treeWalk.getPathString());
+			System.out.println("			found: " + treeWalk.getPathString());
+		}
+
+		walk.dispose();
+
+		return pathList;
 	}
 
 }
