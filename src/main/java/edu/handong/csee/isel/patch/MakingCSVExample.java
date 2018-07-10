aab640ebd7ccc894f3617568d1dc7961efda3246
sungbin <21700613@handong.edu>
2018-07-10 16:14:13 +0900

    I will use this principle to make .csv (Using ArrayList)

--- src/main/java/edu/handong/csee/isel/patch/MakingCSVExample.java
+++ src/main/java/edu/handong/csee/isel/patch/MakingCSVExample.java
@@ -17,18 +17,53 @@ import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvMapWriter;
 import org.supercsv.io.ICsvMapWriter;
 import org.supercsv.prefs.CsvPreference;
+import java.util.ArrayList;
 
 public class MakingCSVExample {
 
 	public static void main(String[] args) throws IOException {
 		// File exampleFile = new File("/Users/imseongbin/Desktop/ExampleOfCsv.csv");
 
-		final String[] header = new String[] { "customerNo", "firstName", "lastName", "birthDate", "mailingAddress",
+		final String[] header = new String[] { "ShortMessage", "firstName", "lastName", "birthDate", "mailingAddress",
 				"married", "numberOfKids", "favouriteQuote", "email", "loyaltyPoints" };
+
+		int i;
+		ArrayList<Map<String, Object>> people = new ArrayList<Map<String, Object>>();
+		for (i = 0; i < 2; i++) {
+			Map<String, Object> temp = new HashMap<String, Object>();
+
+			if (i == 0) {
+				temp.put(header[0], "GGGGGcommit 7323");
+				temp.put(header[1], "temp");
+				temp.put(header[2], "Dunbar");
+				temp.put(header[3], new GregorianCalendar(1945, Calendar.JUNE, 13).getTime());
+				temp.put(header[4], "1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States");
+				temp.put(header[5], null);
+				temp.put(header[6], null);
+				temp.put(header[7], "\"May the Force be with you.\" - Star Wars");
+				temp.put(header[8], "jdunbar@gmail.com");
+				temp.put(header[9], 0L);
+			}
+			
+			if(i == 1) {
+				temp.put(header[0], "2");
+				temp.put(header[1], "temp");
+				temp.put(header[2], "Down");
+				temp.put(header[3], new GregorianCalendar(1919, Calendar.FEBRUARY, 25).getTime());
+				temp.put(header[4], "1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States");
+				temp.put(header[5], true);
+				temp.put(header[6], 0);
+				temp.put(header[7], "\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind");
+				temp.put(header[8], "tempdown@hotmail.com");
+				temp.put(header[9], 123456L);
+			}
+			people.add(temp);
+		}
+		
 
 		// create the customer Maps (using the header elements for the column keys)
 		final Map<String, Object> john = new HashMap<String, Object>();
-		john.put(header[0], "1");
+		john.put(header[0], "GGGGGcommit 7323");
 		john.put(header[1], "John");
 		john.put(header[2], "Dunbar");
 		john.put(header[3], new GregorianCalendar(1945, Calendar.JUNE, 13).getTime());
@@ -62,14 +97,19 @@ public class MakingCSVExample {
 			mapWriter.writeHeader(header);
 
 			// write the customer maps
-			mapWriter.write(john, header, processors);
-			mapWriter.write(bob, header, processors);
+			for(Map<String, Object> person : people) {
+				mapWriter.write(person, header, processors);
+			}
+			
+//			mapWriter.write(john, header, processors);
+//			mapWriter.write(bob, header, processors);
 
 		} finally {
 			if (mapWriter != null) {
 				mapWriter.close();
 			}
 		}
+		System.out.println("Success!");
 
 	}
 
