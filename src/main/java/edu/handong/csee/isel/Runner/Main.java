9ff2f44fc33bf3ca7d42945c76554daa302bba39
sungbin <21700613@handong.edu>
2018-12-19 13:39:22 +0900

    for time checking

--- src/main/java/edu/handong/csee/isel/Runner/Main.java
+++ src/main/java/edu/handong/csee/isel/Runner/Main.java
@@ -48,7 +48,10 @@ public class Main {
 				printHelp(options);
 				return;
 			}
-
+			
+			long start = System.currentTimeMillis();
+			System.out.println("start: "+start/1000.0); //will be removed
+			
 			if (gitRepositoryPath != null) {
 				LocalGitRepositoryPatchCollector gr = new LocalGitRepositoryPatchCollector(gitRepositoryPath,
 						resultDirectory, reference, conditionMax, conditionMin, isThread);
@@ -61,8 +64,9 @@ public class Main {
 				gh.run();
 				long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
 				long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-				System.out.println("시간차이(m) : "+secDiffTime);
 			}
+			long end = System.currentTimeMillis();
+			System.out.println( "실행 시간 : " + ( end - start )/1000.0 );
 
 		}
 	}
