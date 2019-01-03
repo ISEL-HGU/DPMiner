6d172046832ed95edbfb1790fd4b191b81056b15
SUJIN <21500394@handong.edu>
2019-01-03 16:15:21 +0900

    Print commitHash

--- src/main/java/edu/handong/csee/isel/githubcommitparser/GithubPatchCollector.java
+++ src/main/java/edu/handong/csee/isel/githubcommitparser/GithubPatchCollector.java
@@ -24,28 +24,29 @@ public class GithubPatchCollector {
 	}
 
 	public void run() {
-
+////입력받은 깃허브 주소나 파일 주소가 있는지확인 
 		if (address == null && file == null) {
 			return;
 		}
 
 		FileReader fr = new FileReader();
-
+//만약 파일 주소면 파일을 읽는다.
 		if (file != null) {
 			fr.readGithubAddressFile(file);
 		} else {
-			fr.githubAddress.add(address);
+			fr.githubAddress.add(address);//파일을 읽어 주소를 githubAddress에 저장 
 		}
 
 		IssueLinkParser iss = new IssueLinkParser();
 		CommitParser co = new CommitParser();
-		Scanner in = new Scanner(System.in);
+	
 		ArrayList<String> failToAccess = new ArrayList<String>();
 
+		//깃허브 주소 하나를 가지고 issuelinkparser와 commitparser을 돌림 -> (만약 파일이 입력값이라 여러 주소가 있을 경우)다음 주소 접근 
 		for (int i = 0; i < fr.githubAddress.size(); i++) {
 			String oneAddress = fr.githubAddress.get(i);
 
-			try {
+			try { //issueLinkParser 돌림  
 				iss.parseIssueAddress(oneAddress, label);
 				if (iss.issueAddress.size() == 0) {
 
@@ -53,12 +54,15 @@ public class GithubPatchCollector {
 					continue;
 
 				}
+				//CommitParser돌림  
 				co.parseCommitAddress(oneAddress);
-				co.parseAndPrintCommiContents(oneAddress, resultDirectory, conditionMax, conditionMin);
+				//co.parseAndPrintCommiContents(oneAddress, resultDirectory, conditionMax, conditionMin); 이부분은 commitHash출력하는데 필요 없음!
 
 				iss.issueAddress.clear();
 				co.commitAddress.clear();
 				co.commitLine.clear();
+				
+				//arrayList초기화 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
