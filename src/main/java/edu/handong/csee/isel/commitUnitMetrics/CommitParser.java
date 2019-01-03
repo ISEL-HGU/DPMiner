196d6c911cf7583692b26119c14c84c5c41bf4cf
SUJIN <21500394@handong.edu>
2019-01-03 15:36:39 +0900

    Rearrange code

--- src/main/java/edu/handong/csee/isel/commitUnitMetrics/CommitParser.java
+++ src/main/java/edu/handong/csee/isel/commitUnitMetrics/CommitParser.java
@@ -1,9 +1,12 @@
 package edu.handong.csee.isel.commitUnitMetrics;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 
 public class CommitParser {
 	String inputPath;
@@ -14,19 +17,7 @@ public class CommitParser {
 		this.outputPath = resultDirectory;
 	}
 	
-	
-	//커밋 저장
-	//커밋에 해당하는 매트릭 카운트  (소스파일이 여러개 포함되어 있는데 어떻게?)
-	//카운트한 값을 arff형식에 맞춰 프린트 
-	
 	void countCommitMetrics() {
-		try {
-			Repository  rep = new FileRepository(inputPath + "/.git");
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
 	}
-
 }
