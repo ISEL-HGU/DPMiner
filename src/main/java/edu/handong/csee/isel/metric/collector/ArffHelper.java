f5f4c5b7d3735d41fae2a19303a360f495ef1a7f
sungbin <21700613@handong.edu>
2020-01-10 14:43:12 +0900

    make draft to merge two arff. run ArffHelper.java

--- src/main/java/edu/handong/csee/isel/metric/collector/ArffHelper.java
+++ src/main/java/edu/handong/csee/isel/metric/collector/ArffHelper.java
@@ -1,10 +1,17 @@
 package edu.handong.csee.isel.metric.collector;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.core.internal.utils.FileUtil;
 
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
@@ -16,6 +23,9 @@ import weka.filters.unsupervised.attribute.StringToWordVector;
 public class ArffHelper {
 	private String projectName;
 	private String referencePath;
+
+	private final static String attributeNumPatternStr = "(\\{|,)(\\d+)\\s";
+	private final static Pattern attributeNumPattern = Pattern.compile(attributeNumPatternStr);
 
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
@@ -167,5 +177,143 @@ public class ArffHelper {
 		String directoryPath = getMergedDirectoryPath();
 		String path = directoryPath + File.separator + "clean";
 		return new File(path);
+	}
+
+	/*
+	 * main for metric arff parser
+	 */
+	public static void main(String[] args) throws IOException {
+
+		String arffPath1 = "/Users/imseongbin/Desktop/lottie-android.arff";
+		String arffPath2 = "/Users/imseongbin/Desktop/lottie-android.arff";
+		File arff1 = new File(arffPath1);
+		File arff2 = new File(arffPath2);
+
+		File arff3 = makeMergedArff(arff1, arff2);
+
+	}
+
+	private static String mergeData(String oldStr, String newStr) {
+
+		StringBuffer mergedBuf = new StringBuffer();
+
+		mergedBuf.append(oldStr.substring(0, oldStr.indexOf('}')));
+		mergedBuf.append(",");
+		mergedBuf.append(newStr.substring(newStr.indexOf('{') + 1, newStr.indexOf('}')));
+
+		return mergedBuf.toString();
+	}
+
+	private static List<String> plusAttributeSize(List<String> dataLineList, int plusSize) {
+
+		ArrayList<String> dataPlusLineList = new ArrayList<>();
+
+		for (String line : dataLineList) {
+
+			String newLine = plusAttributeSize(line, plusSize);
+			dataPlusLineList.add(newLine);
+		}
+
+		return dataPlusLineList;
+	}
+
+	private static String plusAttributeSize(String line, int plusSize) {
+		StringBuffer newLineBuf = new StringBuffer();
+
+		Matcher m = attributeNumPattern.matcher(line);
+		int lastIndex = 0;
+
+		while (m.find()) {
+
+			int num = Integer.parseInt(m.group(2));
+			num += plusSize;
+
+			int start = m.start(2);
+			int end = m.end(2);
+
+			newLineBuf.append(line.substring(lastIndex, start));
+			newLineBuf.append(num);
+			lastIndex = end;
+		}
+		newLineBuf.append(line.substring(lastIndex, line.length()));
+
+		return newLineBuf.toString();
+	}
+
+	private static File makeMergedArff(File arff1, File arff2) throws IOException {
+		File newFile = new File(arff1.getParentFile().getAbsolutePath() + File.separator + "merged.arff");
+
+		String content1 = FileUtils.readFileToString(arff1, "UTF-8");
+		String content2 = FileUtils.readFileToString(arff2, "UTF-8");
+
+		ArrayList<String> attributeLineList1 = getAttributeLinesFrom(content1);
+		ArrayList<String> attributeLineList2 = getAttributeLinesFrom(content2);
+
+		ArrayList<String> mergedAttributeLineList = new ArrayList<>();
+		mergedAttributeLineList.addAll(attributeLineList1);
+		mergedAttributeLineList.addAll(attributeLineList2);
+
+		ArrayList<String> dataLineList1 = getDataLinesFrom(content1);
+		ArrayList<String> dataLineList2 = getDataLinesFrom(content2);
+
+		int plusAttributeNum = attributeLineList1.size();
+		List<String> dataPlusLineList = plusAttributeSize(dataLineList2, plusAttributeNum);
+
+		List<String> mergedDataLineList = new ArrayList<>();
+
+		for (int i = 0; i < dataLineList1.size(); i++) {
+			String data1 = dataLineList1.get(i);
+			String data2 = dataPlusLineList.get(i);
+			String mergedData = mergeData(data1, data2);
+			mergedDataLineList.add(mergedData);
+		}
+
+		StringBuffer newContentBuf = new StringBuffer();
+
+		for (String line : mergedAttributeLineList) {
+			newContentBuf.append(line + "\n");
+		}
+
+		newContentBuf.append("@data\n");
+
+		for (String line : mergedDataLineList) {
+			newContentBuf.append(line + "\n");
+		}
+
+		FileUtils.write(newFile, newContentBuf.toString(), "UTF-8");
+
+		return newFile;
+	}
+
+	private static ArrayList<String> getDataLinesFrom(String content) {
+		ArrayList<String> dataLineList = new ArrayList<>();
+		String[] lines = content.split("\n");
+
+		boolean dataPart = false;
+		for (String line : lines) {
+			if (dataPart) {
+				dataLineList.add(line);
+
+			} else if (line.startsWith("@data")) {
+
+				dataPart = true;
+			}
+
+		}
+		return dataLineList;
+	}
+
+	private static ArrayList<String> getAttributeLinesFrom(String content) throws IOException {
+		ArrayList<String> attributeLineList = new ArrayList<>();
+
+		String[] lines = content.split("\n");
+
+		for (String line : lines) {
+			if (line.startsWith("@attribute")) {
+				attributeLineList.add(line);
+			}
+		}
+
+		return attributeLineList;
 	}
 }
